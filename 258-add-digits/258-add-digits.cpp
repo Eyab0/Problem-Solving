af450ec8a00724969e47d1c39dd5161d5d1aaab5
Eyab <61092637+Eyab0@users.noreply.github.com>
2022-02-08 17:37:34 +0200

    Time: 4 ms (43.34%), Space: 5.9 MB (25.37%) - LeetHub

---
+++ 258-add-digits/258-add-digits.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int addDigits(int num) {
+        int res=0;
+        while(num!=0){
+            res+=num%10;
+            num/=10;
+        }
+        if(res/10 <1){
+            return res;
+        }else{
+            return addDigits(res);
+        }
+        
+    }
+};
