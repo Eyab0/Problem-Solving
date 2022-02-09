b93fdc0946b7338dc9172ac0d597f652979ccfa1
Eyab <61092637+Eyab0@users.noreply.github.com>
2022-02-10 00:58:20 +0200

    Time: 4 ms (48.39%), Space: 8.8 MB (73.03%) - LeetHub

--- 27-remove-element/27-remove-element.cpp
+++ 27-remove-element/27-remove-element.cpp
@@ -2,15 +2,12 @@ class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
         
-        int k=nums.size();
-        for(int i=0;i<k;i++){
+        
+        int j=0;
+        for(int i=0;i<nums.size();i++){
             
-            if(nums[i]==val){
-                for(int j=i+1;j<nums.size();j++){
-                    nums[j-1] = nums[j];
-                }
-                i--;
-                k--;
+            if(nums[i]!=val){
+                swap(nums[i],nums[j++]);
             }
             
             
@@ -18,8 +15,38 @@ public:
             
         }
         
+        return j;
         
-        return k;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        //another sol.
+        
+//         int k=nums.size();
+//         for(int i=0;i<k;i++){
+            
+//             if(nums[i]==val){
+//                 for(int j=i+1;j<nums.size();j++){
+//                     nums[j-1] = nums[j];
+//                 }
+//                 i--;
+//                 k--;
+//             }
+            
+            
+            
+            
+//         }
+        
+        
+//         return k;
         
         
         
